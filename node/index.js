410fe58e60e4aac3dfda172ceaee56661372735c
WMAlex <karel-.-alex@hotmail.com>
2024-03-05 12:04:35 -0600

    Primer commit, integrar ejercicios javascripting

---
+++ node/index.js
@@ -0,0 +1,22 @@
+// Crear una funcion.
+function sumNum(a,b){
+    return a+b;
+}
+
+console.log('sum',sumNum(10,7));
+
+// Representacion de datos por "flecha", una opcion para almacenar datos
+const resNum =(c,d) =>{ 
+    return c-d;
+
+}
+// console.log, impresion en la consola. 
+console.log('result_res',resNum(10,7));
+
+// tipos de variables
+
+let algo;  //Es principalmete para variables globales 
+var algo2;   // El mismo var de siempre
+const algo3 = 1; // siempre se debe de dar un valor   
+
+
