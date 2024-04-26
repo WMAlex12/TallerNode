e71a9827205656c2a228da793482ac2655fdb351
WMAlex <karel-.-alex@hotmail.com>
2024-04-26 08:08:46 -0600

    Agregando ruta /myname

--- Actividad1/Index.js
+++ Actividad1/Index.js
@@ -1,2 +1,23 @@
-const express = require('express')
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(`Hello World from Port ${port}`);
+});
+
+app.get('/myname', (req, res) => {
+    const name = 'KarelVazquez'; 
+    res.send(`Hello World! My name is ${name}.`);
+  });
+  
+
+app.listen(3000, () =>{
+    console.info(`Database is running on port ${port}`);
+});
 
